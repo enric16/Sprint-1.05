35ec8afd47087471efe661ef088de71e4f80041e
enricSalvador <esalvador94@gmail.com>
2025-04-01 12:59:15 +0200

    Revision post correction

--- Sprint-1.05 Nivell1 Dog.php
+++ Sprint-1.05 Nivell1 Dog.php
@@ -2,10 +2,6 @@
 
 class Dog extends Animal {
 
-    public function __construct(string $nom) {
-        parent::__construct($nom);
-    }
-
     public function makeSound() {
         echo "El gos $this->nom diu: Bup, bup!" . "<br>";             
     }
